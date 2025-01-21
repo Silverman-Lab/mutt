1c8284e44471e010177d4baf291c10a977789cb9
Won Gu <95193620+wg99526@users.noreply.github.com>
2025-01-21 09:45:13 -0500

    add contijoch metagenomics

--- 2019_contijoch_eLife_5data16S/parse.R
+++ 2019_contijoch_eLife_5data16S/parse.R
@@ -1,5 +1,6 @@
 library(readxl)
 library(biomformat)
+library(readr)
 parse_2019_contijoch_eLife_5data16S <- function(paths = NULL) {
   local <- file.path("2019_contijoch_eLife_5data16S/")
   
@@ -76,33 +77,6 @@ parse_2019_contijoch_eLife_5data16S <- function(paths = NULL) {
   scale4.2 <- data.frame(total = metadata4.2$`Sample Mass` * (metadata4.2$`Microbiota Density` * 10^-6) / (10^-15))
   rownames(scale4.2) <- metadata4.2$SampleID
   
-  # metadata1.2 <- read_xlsx(paste0(local,"elife-40553-fig1-data2-v1.xlsx"), skip = 2)
-  # metadata1.2$SampleID <- str_replace_all(metadata1.2$SampleID, "-|_", ".")
-  # dim(metadata1.2)
-  # sum(metadata1.2$SampleID %in% colnames(counts)) # none in counts
-  
-  #metadata2.2 <- read_xlsx(paste0(local,"elife-40553-fig2-data2-v1.xlsx"), skip = 2) # didn't include; not in counts
-  #metadata2.2$SampleID <- str_replace_all(metadata2.2$SampleID, "-|_", ".")
-  #dim(metadata2.2)
-  #sum(metadata2.2$SampleID %in% colnames(counts)) # all match
-  
-  # metadata2.3 <- read_xlsx(paste0(local,"elife-40553-fig2-data3-v1.xlsx"), skip = 2)
-  # metadata2.3$SampleID <- str_replace_all(metadata2.3$SampleID, "-|_", ".")
-  # dim(metadata2.3)
-  # sum(metadata2.3$SampleID %in% colnames(counts)) # don't exist in counts
-  
-  # metadata2.4 <- read_xlsx(paste0(local,"elife-40553-fig2-data4-v1.xlsx"), skip = 2)
-  # metadata2.4$SampleID <- str_replace_all(metadata2.4$SampleID, "-|_", ".")
-  # dim(metadata2.4)
-  # sum(metadata2.4$SampleID %in% colnames(counts)) # don't exist in counts
-  
-  # metadata3.1 <- read_xlsx(paste0(local,"elife-40553-fig3-data1-v1.xlsx"), skip = 2)
-  # metadata3.1$SampleID <- str_replace_all(metadata3.1$SampleID, "-|_", ".")
-  # dim(metadata3.1)
-  # sum(metadata3.1$SampleID %in% colnames(counts)) # don't exist in counts
-  
-  # but what's the samples start with ABP? It exist in biom file but not in any of metadata
-  
   # tax - note from author ; 
   # OTU ID in these samples corresponds to those from the (now no longer maintained) greengenes database gg_13_8, clustered at 97% similarity,
   # note; gg_13_8_99 is used instead (couldn't find gg_13_8_97)
@@ -146,59 +120,3 @@ parse_2019_contijoch_eLife_5data16S <- function(paths = NULL) {
   
   return(list(list(scale=scale, metadata=metadata, counts=counts, tax=tax)))
 }
-
-
-# library(readxl)
-# parse_2019_contijoch_eLife_metagenomic <- function(paths = NULL) {
-#   local <- file.path("/storage/work/wpg5129/sparse_project/2019_contijoch_eLife/")
-#   
-#   # data associated with FMT in patients with rCDI : metagenomic data (data 4) n=15
-#   metadata <- read_xlsx(paste0(local,"elife-40553-fig4-data1-v1.xlsx"), skip = 2)
-#   
-#   # other datas
-#   counts <- read_table(paste0(local, "metagenomics_combined_full.txt"))
-#   
-#   # Define the ranks in order
-#   ranks <- c("kingdom", "phylum", "class", "order", "family", "genus", "species")
-#   
-#   # Split strings into components
-#   tax_split <- strsplit(counts$Taxa, "\\|")
-#   
-#   # Create a matrix with 7 columns
-#   tax_matrix <- do.call(rbind, lapply(tax_split, function(entry) {
-#     # Extract ranks, ensuring a length of 7 (filling with NA if missing)
-#     ranks <- sapply(ranks, function(rank) {
-#       matched <- grep(paste0("^", substr(rank, 1, 1), "__"), entry, value = TRUE)
-#       if (length(matched) == 0) return(NA) else return(sub("^[a-z]__", "", matched))
-#     })
-#     return(ranks)
-#   }))
-#   
-#   # Convert to data frame for better visualization
-#   tax <- as.data.frame(tax_matrix, stringsAsFactors = FALSE)
-#   colnames(tax) <- ranks
-#   rownames(tax) <- paste0("seq", 1:nrow(counts))
-#   
-#   
-#   ####
-#   scale <- metadata$`Sample Mass` * metadata$`Microbiota Density`
-#   rownames(metadata) <- metadata$SampleID
-#   
-#   samID <- idmap$SampleID[match(scale$Sample, idmap$SampleNumber)]
-#   scale <- scale[samID %in% rownames(metadata),]
-#   scale$SampleID <- samID[samID %in% rownames(metadata)]
-#   
-#   counts <- counts[,match(rownames(metadata), colnames(counts))]
-#   
-#   #remove samples that doesn't match
-#   counts <- counts[,colnames(counts) %in% rownames(metadata)]
-#   metadata <- metadata[metadata$SampleID %in% colnames(counts),]
-#   rownames(metadata) <- metadata$SampleID
-#   scale <- scale[scale$SampleID %in% colnames(counts),]
-#   
-#   all.equal(colnames(counts), rownames(metadata))
-#   all.equal(colnames(counts), scale$SampleID)
-#   all.equal(rownames(metadata), scale$SampleID)
-#   
-#   return(list(scale=scale, metadata=metadata, counts=counts, tax=tax))
-# }
