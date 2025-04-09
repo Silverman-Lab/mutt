72d32a46b713e50d0294b2113e93a1aa161bc9d8
Manan Saxena <32758106+manansaxena@users.noreply.github.com>
2025-04-09 18:43:17 +0000

    parse script for 2024_jin_pnas_semen

--- 2024_jin_pnas_semen/parse.R
+++ 2024_jin_pnas_semen/parse.R
@@ -1,26 +1,32 @@
-library(stringr)
+require(stringr)
 library(readxl)
-parse_2024_jin_pnas_semen <- function(paths = NULL) {
-  local <- file.path("/storage/work/wpg5129/sparse_project","2024_jin_pnas_semen")
+library(dplyr)
+
+# removing the control samples as no scale available for them 
+parse_2024_jin_pnas_semen <- function(rootPath = getwd(), paths = NULLL) {
+  localPath <- paste0(rootPath, "/2024_jin_pnas_semen/")
   
-  metadata <- as.data.frame(read_table(paste0(local, "/metadata4.txt")))
+  metadata <- as.data.frame(read_table(paste0(localPath, "metadata4.txt")))
   rownames(metadata) <- metadata$`sample-ID`
+  metadata <- subset(metadata, select = -`sample-ID`)
+  metadata <- metadata[!(rownames(metadata) %in% c("S1", "S13", "SN", "SNTC")), ]
   
-  counts <- as.data.frame(read_tsv(paste0(local, "/table.tsv"), skip = 1))
+  
+  counts <- as.data.frame(read_tsv(paste0(localPath, "table.tsv"), skip = 1))
   rownames(counts) <- counts$`#OTU ID`
+  counts <- subset(counts, select = -`#OTU ID`)
+  counts <- t(counts)
+  counts <- counts[!(rownames(counts) %in% c("S1", "SN", "SNTC")), ]
   
-  tax <- read_tsv(paste0(local, "/taxonomy.tsv"))
+  
+  tax <- as.data.frame(read_tsv(paste0(localPath, "taxonomy.tsv")), skip = 1)
   rownames(tax) <- tax$`Feature ID`
+  tax <- subset(tax, select = -`Feature ID`)
   
-  scale <- read_table(paste0(local, "/CFU_data.txt"))
+  scale <- as.data.frame(read_table(paste0(localPath, "CFU_data.txt")), skip = 1)
   rownames(scale) <- scale$`sample-ID`
-  
-  ## TODO some samples do not match between metadata and counts
-  counts <- counts[,colnames(counts) %in% rownames(metadata)]
-  metadata <- metadata[rownames(metadata) %in% colnames(counts),]
-  
-  all.equal(colnames(counts), rownames(metadata))
+  scale <- subset(scale, select = -`sample-ID`)
+  scale <- t(scale)
   
   return(list(scale=scale, metadata=metadata, counts=counts, tax=tax))
 }
-
